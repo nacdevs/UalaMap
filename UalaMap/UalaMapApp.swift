ad8c7b9bccbccc0217cfd019cd2c770313e432f7
Nestor Camela <nestorcamela@gmail.com>
2025-06-16 15:07:13 -0300

    Initial Commit

---
+++ UalaMap/UalaMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  UalaMapApp.swift
+//  UalaMap
+//
+//  Created by Nestor Camela on 16/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct UalaMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UalaMapTests/UalaMapTests.swift
@@ -0,0 +1,17 @@
+//
+//  UalaMapTests.swift
+//  UalaMapTests
+//
+//  Created by Nestor Camela on 16/06/2025.
+//
+
+import Testing
+@testable import UalaMap
+
+struct UalaMapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
